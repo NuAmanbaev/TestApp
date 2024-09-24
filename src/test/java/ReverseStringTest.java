6d067a7b3a541317803323c5b7e94e607b6be785
Nurgazy Amanbaev <nurgazyamanbaev@Nurgazys-Air.hsd1.il.comcast.net>
2024-09-23 20:13:19 -0500

    first commit

---
+++ src/test/java/ReverseStringTest.java
@@ -0,0 +1,27 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReverseStringTest {
+
+    @Test
+    public void reverseStringWithnull(){
+       Assert.assertNull(ReverseString.reverseString(null));
+
+
+
+    }
+    @Test
+    public void reverseWithDigits(){
+        Assert.assertEquals("aza", ReverseString.reverseString("aza"));
+        System.out.println("reverseStringDigits pass");
+    }
+
+
+
+    @Test
+    public void  reverseTestHappyPath(){
+        Assert.assertEquals("ananab", ReverseString.reverseString("banana"));
+        System.out.println("reverseStringHappyPath pass");
+    }
+
+}
